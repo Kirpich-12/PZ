253138dbdc65e8614be81abb80091f5aa9df0950
Kirpich-12 <ponchik34@tuta.io>
2024-12-13 14:43:40 +0300

    I do this

--- rev.py
+++ rev.py
@@ -6,10 +6,7 @@ def reverse(st:str)-> str:
 print(reverse(a))
 
 def reverse_rc(st:str) -> str:
-    if st == a[::-1]:
-        return st
-    else:
-        st = st[-1] + st [:-1]
-        return reverse_rc(st)
-
+    if len(st) == 1:
+       return st
+    return reverse_rc(st[1:]) + st[0]
 print(reverse_rc(a))
--- tt.py
+++ tt.py
@@ -4,5 +4,5 @@ for i in range(5):
     if st == a[::-1]:
         print(st)
     else:
-        st = st[1:]] + st [:-1]
+        st = st[1:] + st [:-1]
     print(st)
